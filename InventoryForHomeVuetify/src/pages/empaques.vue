79f3cf2c99f76f3d902d30d1336cc2e0a5534eee
Yazbeth <yazryrg@outlook.com>
2024-11-05 22:38:23 -0600

    +Solución de errores de las tablas main de Emp y Prio

--- InventoryForHomeVuetify/src/pages/empaques.vue
+++ InventoryForHomeVuetify/src/pages/empaques.vue
@@ -21,14 +21,17 @@
         </v-btn>
     </v-container>
     <v-container>
-        <v-data-table :headers="headers" :items="items" class="elevation-1">
-            <template #item.id="{ item }">
-                {{ item.id }}
-            </template>
-            <template #item.type="{ item }">
-                {{ item.type }}
-            </template>
-        </v-data-table>
+    <v-data-table
+    :headers="headers"
+    :items="mainEmp"
+    :items-per-page="10"
+    class="elevation-1"
+  >
+  <template #item.active="{ item }">
+      <v-icon v-if="item.active" color="green">mdi-check-circle</v-icon>
+      <v-icon v-else color="red">mdi-close-circle</v-icon>
+    </template>
+  </v-data-table>
     </v-container>
     <FormEmpC></FormEmpC>
     <FormEmpR></FormEmpR>
@@ -37,20 +40,56 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
+import ApiService from '@/services/apiServices'; //ME TRAIGO EL SERVICIO PARA CONSUMIR API
+
+interface empMain { //estructura de la información de la tabla
+    IdTypeStock: number;
+    TypeStockName: string;
+    active: boolean;
+}
+
+interface empApiMain { //estructura del objeto que se trae del api
+    success: boolean;
+    data: empMain [];
+}
+
+const responseAPI = ref<empApiMain>(); //INSTANCIA NUEVA DE RESPUETA API
+const mainEmp = ref<empMain[]>([]); //LA LISTA DE OBJETOS QUE VOY A MONTAR EN MIS INTERFACES
+
+//funcion para traer la info de la tabla
+const getEmpaques = async () =>
+{
+    try 
+    {
+        responseAPI.value = await ApiService.getData<empApiMain>('Empaques/ReadEmps');
+        // console.log(responseAPI.value);
+        mainEmp.value = responseAPI.value.data;
+    }
+    catch(error)
+    {
+        console.error('ERROR AL TRAER DATOS EN',error);
+    }
+    finally
+    {
+    }
+}
+
+onBeforeMount(getEmpaques)
 
 // Definimos las cabeceras de la tabla
 const headers = ref([
-    { text: 'ID', value: 'id' },
-    { text: 'Tipo de empaques', value: 'type' }
+    { text: 'ID', value: 'idTypeStock'},
+    { text: 'Tipo de empaques', value: 'typeStockName'}
+    // { text: 'activo', value: 'active'}
 ]);
 
 // Definimos los datos de los empaques
-const items = ref([
-    { id: 1, type: 'Caja' },
-    { id: 2, type: 'Bolsa' },
-    { id: 3, type: 'Palet' }
-]);
+// const items = ref([
+//     { id: 1, type: 'Caja' },
+//     { id: 2, type: 'Bolsa' },
+//     { id: 3, type: 'Palet' }
+// ]);
 
 function handleButtonClick(buttonName: string) {
     alert(`¡${buttonName} clickeado!`);
--- InventoryForHomeVuetify/src/pages/prioridades.vue
+++ InventoryForHomeVuetify/src/pages/prioridades.vue
@@ -1,66 +1,84 @@
 <template>
-    <v-container>
-        <!-- Botón 1 -->
-        <v-btn color="primary" variant="outlined" @click="handleButtonClick('Botón 1')">
-            Crear nueva regla
-        </v-btn>
+  <v-container>
+    <!-- Botón 1 -->
+    <v-btn color="primary" variant="outlined" @click="handleButtonClick('Botón 1')">
+      Crear nueva regla
+    </v-btn>
 
-        <!-- Botón 2 -->
-        <v-btn color="secondary" variant="outlined" @click="handleButtonClick('Botón 2')">
-            Buscar por Id
-        </v-btn>
+    <!-- Botón 2 -->
+    <v-btn color="secondary" variant="outlined" @click="handleButtonClick('Botón 2')">
+      Buscar por Id
+    </v-btn>
 
-        <!-- Botón 3 -->
-        <v-btn color="success" variant="outlined" @click="handleButtonClick('Botón 3')">
-            Editar elemento
-        </v-btn>
+    <!-- Botón 3 -->
+    <v-btn color="success" variant="outlined" @click="handleButtonClick('Botón 3')">
+      Editar elemento
+    </v-btn>
 
-        <!-- Botón 4 -->
-        <v-btn color="error" variant="outlined" @click="handleButtonClick('Botón 4')">
-            Borrar elemento
-        </v-btn>
-    </v-container>
-    <v-container>
-        <v-data-table :headers="headers" :items="priorityRules" class="elevation-1" item-value="id" :items-per-page="5"
-            hide-default-footer>
-        </v-data-table>
-    </v-container>
-    <FormPrioC></FormPrioC>
-    <FormPrioR></FormPrioR>
-    <FormPrioU></FormPrioU>
-    <FormPrioD></FormPrioD>
+    <!-- Botón 4 -->
+    <v-btn color="error" variant="outlined" @click="handleButtonClick('Botón 4')">
+      Borrar elemento
+    </v-btn>
+  </v-container>
+  <v-container>
+    <v-data-table v-if="mainPrio.length > 0" :headers="headers" :items="mainPrio" item-key="idTypePrioritary"
+      class="elevation-1">
+      <template v-slot:item.active="{ item }">
+        <v-icon :color="item.active ? 'green' : 'red'">
+          {{ item.active ? 'mdi-check-circle' : 'mdi-close-circle' }}
+        </v-icon>
+      </template>
+    </v-data-table>
+  </v-container>
+  <FormPrioC></FormPrioC>
+  <FormPrioR></FormPrioR>
+  <FormPrioU></FormPrioU>
+  <FormPrioD></FormPrioD>
 </template>
   
   <script setup lang="ts">
-  import { ref } from 'vue';
+  import { onBeforeMount, ref } from 'vue';
+  import ApiService from '@/services/apiServices'; //ME TRAIGO EL SERVICIO PARA CONSUMIR API
+
+  interface prioMain {
+    IdTypePrioritary: number;
+    TypePrioritaryName: string;
+    Description: string;
+    active: boolean;
+  }
+
+  interface prioApiMain { //estructura del objeto que se trae del api
+    success: boolean;
+    data: prioMain[];
+  }
+
+  const responseAPI = ref<prioApiMain>(); //INSTANCIA NUEVA DE RESPUETA API
+  const mainPrio = ref<prioMain[]>([]); //LA LISTA DE OBJETOS QUE VOY A MONTAR EN MIS INTERFACES
+
+  //funcion para traer la info de la tabla
+  const getPrioridades = async () => {
+    try {
+      responseAPI.value = await ApiService.getData<prioApiMain>('Prioridades/ReadPrios');
+      // console.log(responseAPI.value);
+      mainPrio.value = responseAPI.value.data;
+    }
+    catch (error) {
+      console.error('ERROR AL TRAER DATOS EN', error);
+    }
+    finally {
+    }
+  }
+
+  onBeforeMount(getPrioridades)
   
   // Definir las cabeceras de las columnas
   const headers = [
-    { text: 'ID', value: 'id', align: 'start' },
-    { text: 'Regla de prioridad', value: 'priorityRule', align: 'start' },
-    { text: 'Descripción', value: 'description', align: 'start' },
-  ];
+      { text: 'ID', value: 'idTypePrioritary' },
+      { text: 'Nombre', value: 'typePrioritaryName' },
+      { text: 'Descripción', value: '_Description' },
+      // { text: 'Activo', value: 'active' },
+    ];
   
-  // Datos de ejemplo para las reglas de prioridad
-  const priorityRules = ref([
-    {
-      id: 1,
-      priorityRule: 'Alta',
-      description: 'Prioridad alta, requiere atención inmediata',
-    },
-    {
-      id: 2,
-      priorityRule: 'Media',
-      description: 'Prioridad media, importante pero no urgente',
-    },
-    {
-      id: 3,
-      priorityRule: 'Baja',
-      description: 'Prioridad baja, puede ser atendido en cualquier momento',
-    },
-    // Agrega más objetos según necesites
-  ]);
-
   function handleButtonClick(buttonName: string) {
   alert(`¡${buttonName} clickeado!`);
 }
